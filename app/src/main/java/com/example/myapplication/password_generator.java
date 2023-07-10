62b674d0fd2a421df69001efc39739d736859c6c
ADRDE <adrdeproject@gmail.com>
2023-07-10 09:53:44 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/password_generator.java
@@ -0,0 +1,15 @@
+
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class password_generator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_generator);
+    }
+}
